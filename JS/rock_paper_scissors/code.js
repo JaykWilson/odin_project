b60a9f75f3f3c1dd2b3477f8ba67d97dbe4e003c
Jayk <jaykwilson1314@gmail.com>
2022-07-08 00:02:01 -0500

    improve playability

--- JS/rock_paper_scissors/code.js
+++ JS/rock_paper_scissors/code.js
@@ -1,14 +1,19 @@
 var hands = ["rock", "paper", "scissors"];
+var upperHand = {"rock":"scissors", "scissors":"paper","paper":"rock"};
 var myScore = 0;
+var pcScore = 0;
 var roundsPlayed = 0;
-rpsbuttons = document.querySelectorAll("button");
-for (button of rpsbuttons)
+scoreLabel = document.querySelector('#scorelabel');
+
+
+var rpsButtons = document.querySelectorAll('button');
+for (var i = 0; i < rpsButtons.length; i++)
 {
-	button.addEventListener('click', function(button) {
-		playRound(button, computerPlay());
+	rpsButtons[i].addEventListener('click', function() {
+		playRound(this.id, computerPlay());
+		checkReset();
 	});
 }
-
 
 function computerPlay(){
 	// returns a random hand
@@ -17,29 +22,58 @@ function computerPlay(){
 
 function playRound(playerSelection, computerSelection) {
 	// determines winner
-
 	if (upperHand[computerSelection] == playerSelection) {
 		roundsPlayed++;
 		pcScore++;
-		alert(`You Lose! ${computerSelection} beats ${playerSelection}`);
+		showHand(playerSelection, computerSelection);
+		updateScore();
+		scoreLabel.style.color = "red";
+		console.log(`You lost that hand! ${computerSelection} beats ${playerSelection}`);
 	} else if (upperHand[playerSelection] == computerSelection) {
 		roundsPlayed++;
 		myScore++;
-		alert(`You Win! ${playerSelection} beats ${computerSelection}`);
+		showHand(playerSelection, computerSelection);
+		updateScore();
+		scoreLabel.style.color = "green";
+		console.log(`You Won that hand! ${playerSelection} beats ${computerSelection}`);
 	} else {
 		roundsPlayed++;
-		alert('Tie! LOL!!!!!!!!!');
-	}
-
-	if (roundsPlayed == 5)
-	{
-		alert(myScore > pcScore ? "You won the game!" : "You lost the game!");
-		// roundsPlayed = 0;
-		// pcScore = 0;
-		// myScore = 0;
+		showHand(playerSelection, computerSelection);
+		updateScore();
+		scoreLabel.style.color = "white";
+		console.log("Tie");
 	}
 }
 
+function showHand(playerSelection, computerSelection){
+	myHand = document.querySelector('#myHand');
+	myHand.textContent = playerSelection;
+	pcHand = document.querySelector('#pcHand');
+	pcHand.textContent = computerSelection;
+	
+}
+function updateScore()
+{
+	myScoreElem = document.querySelector('#my-score');
+	myScoreElem.textContent = myScore;
+	pcScoreElem = document.querySelector('#pc-score');
+	pcScoreElem.textContent = pcScore;
+	
+}
+
+function checkReset() {
+	if (roundsPlayed == 5)
+	{
+		alert(myScore > pcScore ? "You won the game!" : myScore < pcScore ? "You lost the game!" : "Tie!");
+		roundsPlayed = 0;
+		pcScore = 0;
+		myScore = 0;
+		updateScore();
+		showHand("","");
+		scoreLabel.style.color = "white";
+	}
+
+}
 
 
 
